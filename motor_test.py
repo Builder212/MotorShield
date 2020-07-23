26a654afb843401e9fa3e486dfccb2e8338e1d8c
Builder212 <building2day@gmail.com>
2020-07-23 15:18:21 -0400

    Created new test for the motors.

--- motor_test.py
+++ motor_test.py
@@ -1,2 +1,15 @@
 from drive_shield/arrow_shield import arrow
-from drive_shield/motor_shield import motor
+from drive_shield/motor_shield import motor, motor_set
+import RPi.GPIO as GPIO
+from time import sleep
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BOARD)
+    m1 = motor("motor_1", 2)
+    m2 = motor("motor_2", 1)
+    m3 = motor("motor_3", 1)
+    m4 = motor("motor_4", 1)
+    drive = motor_set(m1, m2, m3, m4)
+    drive.forward(100)
+    time.sleep(10)
+    drive.stop()
